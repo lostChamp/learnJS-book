5f5bdcde819bfeb85ac54905fa41c1a19f76de0c
Kostya <lost.champ2007@gmail.com>
2023-03-22 23:17:49 +0500

    LearnJS Book 3Stage

---
+++ 12 глава/12.1.Task1.js
@@ -0,0 +1,14 @@
+function* pseudoRandom(seed) {
+    let value = seed;
+
+    while(true) {
+        value = value * 16807 % 2147483647;
+        yield value;
+    }
+}
+
+let generator = pseudoRandom(1);
+
+console.log(generator.next().value); // 16807
+console.log(generator.next().value); // 282475249
+console.log(generator.next().value); // 1622650073
